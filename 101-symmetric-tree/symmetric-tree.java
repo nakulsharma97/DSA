3f0a7b9ff847d1ef6c541fbfc6978edb078d10ef
nakulsharma97 <nakulsharma978397@gmail.com>
2025-12-15 17:52:53 +0530

    Time: 1 ms (12.26%) | Memory: 43.6 MB (16.76%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,19 +15,21 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-        if (root == null)
-            return true;
-        return height(root.left, root.right);
-
+         if(root == null) return true  ;
+      Queue<TreeNode> q = new LinkedList<>() ;
+      q.offer(root.left) ;
+      q.offer(root.right) ;
+      while(!q.isEmpty()){
+       TreeNode node = q.poll() ;
+       TreeNode node1 = q.poll() ;
+       if(node == null && node1 == null) continue  ;
+       if(node == null || node1 == null) return false ;
+        if(node.val != node1.val) return false  ;
+        q.offer(node.left) ;
+        q.offer(node1.right) ;
+        q.offer(node.right) ;
+        q.offer(node1.left) ;
+      }  
+      return true ;
     }
-
-    public boolean height(TreeNode p, TreeNode q) {
-        if (p == null && q == null)
-            return true;
-        if (p == null || q == null)
-            return false;
-        return (p.val == q.val) && height(p.left, q.right) && height(p.right, q.left);
-
-    }
-
 }
