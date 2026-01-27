d32fd9a9582f2571f2328fd0908b9d3fc4bc432c
nakulsharma97 <nakulsharma978397@gmail.com>
2026-01-27 22:34:41 +0530

    Time: 1 ms (45.06%) | Memory: 46.7 MB (85.73%) - LeetSync

--- 733-flood-fill/flood-fill.java
+++ 733-flood-fill/flood-fill.java
@@ -1,36 +1,37 @@
 class Solution {
-    int rows , cols ;
-    void dfs(int row ,int col , int color , int curcolor,boolean visited[][] , int image[][]){
-        // out of bound 
-        if(row <0 || row >= rows || col < 0 || col >= cols){
-            return ;
-        }
-        // if color not present 
-        if(image[row][col] != curcolor){
-            return ;
-        }
-        // node already visited
-        if(visited[row][col]){
-            return ;
-        }
-        image[row][col] = color ;
-        visited[row][col] = true ;
-        // neighbours 
-        int adjlist[][] = {
-            {row-1, col}, {row , col+1},{ row+1,col}, {row, col -1} 
-        } ;
-        for(int  nei[] :  adjlist){
-            dfs( nei[0] ,nei[1] ,  color ,  curcolor,visited, image) ;
-        }
+    int rows, cols;
 
-
-
-    }
     public int[][] floodFill(int[][] image, int sr, int sc, int color) {
         rows = image.length;
-        cols = image[0].length ;
-        boolean[][] visited = new boolean[rows][cols] ;
-        dfs(sr, sc ,color , image[sr][sc], visited, image ) ;
-        return image ;
+        cols = image[0].length;
+        int curcolor = image[sr][sc];
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{sr, sc});
+        image[sr][sc] = color; // fill as well as visit 
+        while (!q.isEmpty()) {
+            int node[] = q.poll();
+            int row = node[0];
+            int col = node[1];
+            int adjlist[][] = {
+                    { row - 1, col }, { row, col + 1 }, { row + 1, col }, { row, col - 1 }
+            };
+            for (int nei[] : adjlist) {
+                int r = nei[0];
+                int c = nei[1];
+                // out of bound 
+                if (r < 0 || r >= rows || c < 0 || c >= cols) {
+                    continue;
+                }
+                // if color not present 
+                if (image[r][c] != curcolor || image[r][c] == color) {
+                    continue;
+                }
+                
+             q.offer(new int[]{r,c}) ;
+             image[r][c] = color ;
+            }
+        }
+       
+        return image;
     }
 }
