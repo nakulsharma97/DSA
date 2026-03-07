089394234bbfbffa888546238da0531d8054bd8d
nakulsharma97 <nakulsharma978397@gmail.com>
2026-03-07 21:31:46 +0530

    Time: 28 ms (36.86%) | Memory: 47.3 MB (66.95%) - LeetSync

--- 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
+++ 2017-minimum-number-of-flips-to-make-the-binary-string-alternating/minimum-number-of-flips-to-make-the-binary-string-alternating.java
@@ -1,18 +1,31 @@
 class Solution {
     public int minFlips(String s) {
         int n = s.length();
-        int res = n;
-        int[] op = {0, 0};
+        String t = s + s;
 
-        for (int i = 0; i < n; i++)
-            op[(s.charAt(i) ^ i) & 1]++;
+        int diff1 = 0, diff2 = 0;
+        int ans = Integer.MAX_VALUE;
 
-        for (int i = 0; i < n; i++) {
-            op[(s.charAt(i) ^ i) & 1]--;
-            op[(s.charAt(i) ^ (n + i)) & 1]++;
-            res = Math.min(res, Math.min(op[0], op[1]));
+        for (int i = 0; i < 2 * n; i++) {
+            char expect1 = (i % 2 == 0) ? '0' : '1'; // 0101
+            char expect2 = (i % 2 == 0) ? '1' : '0'; // 1010...
+
+            if (t.charAt(i) != expect1) diff1++;
+            if (t.charAt(i) != expect2) diff2++;
+
+            if (i >= n) {
+                char oldExpect1 = ((i - n) % 2 == 0) ? '0' : '1';
+                char oldExpect2 = ((i - n) % 2 == 0) ? '1' : '0';
+
+                if (t.charAt(i - n) != oldExpect1) diff1--;
+                if (t.charAt(i - n) != oldExpect2) diff2--;
+            }
+
+            if (i >= n - 1) {
+                ans = Math.min(ans, Math.min(diff1 , diff2));
+            }
         }
 
-        return res;
+        return ans;
     }
 }
