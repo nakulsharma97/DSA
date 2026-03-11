9e30e0d615e38923be7ad1c3a0c17b1b2f93fbb3
nakulsharma97 <nakulsharma978397@gmail.com>
2026-03-11 09:46:36 +0530

    Time: 1 ms (99.75%) | Memory: 48.7 MB (85.34%) - LeetSync

--- 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
+++ 4-median-of-two-sorted-arrays/median-of-two-sorted-arrays.java
@@ -1,41 +1,42 @@
-import java.util.Arrays;
 class Solution {
     public double findMedianSortedArrays(int[] nums1, int[] nums2) {
-        double med = 0;
-int i = 0 ;
-int j = 0 ;
-int k = 0 ;
 
-        int n = nums1.length ;
-        int n1 = nums2.length ;
-        int [] result = new int[n + n1] ;
-        while(i < n && j < n1 ){
-            if(nums1[i] > nums2[j]){
-                result[k] = nums2[j] ;
-                j++ ;
-            }
-            else {
-                result[k] = nums1[i] ;
-                i++ ;
-            }
-            k++ ;
-
+        // Ensure nums1 is the smaller array
+        if (nums1.length > nums2.length) {
+            return findMedianSortedArrays(nums2, nums1);
         }
-while (i < n){
-    result[k] = nums1[i] ;
-    i++ ;
-    k++;
-}
-while( j < n1){
-    result[k] = nums2[j] ;
-    j++;
-    k++ ;
-}
-if(k % 2 != 0){
-    return result[k / 2] ;
-} 
- 
-    return ( result[ (k -1) / 2]  + result[k / 2] ) / 2.0;
- 
+
+        int n = nums1.length;
+        int m = nums2.length;
+
+        int low = 0, high = n;
+
+        while (low <= high) {
+
+            int cut1 = (low + high) / 2;
+            int cut2 = (n + m + 1) / 2 - cut1;
+
+            int L1 = (cut1 == 0) ? Integer.MIN_VALUE : nums1[cut1 - 1];
+            int R1 = (cut1 == n) ? Integer.MAX_VALUE : nums1[cut1];
+
+            int L2 = (cut2 == 0) ? Integer.MIN_VALUE : nums2[cut2 - 1];
+            int R2 = (cut2 == m) ? Integer.MAX_VALUE : nums2[cut2];
+
+            if (L1 <= R2 && L2 <= R1) {
+
+                if ((n + m) % 2 == 0) {
+                    return (Math.max(L1, L2) + Math.min(R1, R2)) / 2.0;
+                } else {
+                    return Math.max(L1, L2);
+                }
+
+            } else if (L1 > R2) {
+                high = cut1 - 1;
+            } else {
+                low = cut1 + 1;
+            }
+        }
+
+        return 0.0;
     }
 }
