936527e286f39d05252154062c4984879dbf8168
Nakul Sharma <nakulsharma978397@gmail.com>
2026-03-14 12:23:15 +0530

    Time: 46 ms (42.57%) | Memory: 47.9 MB (35.64%) - LeetSync

--- 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
+++ 3496-minimum-number-of-seconds-to-make-mountain-height-zero/minimum-number-of-seconds-to-make-mountain-height-zero.java
@@ -1,50 +1,48 @@
 class Solution {
+    public boolean isPossible(long time, int height, int[] worker) {
 
-    // Function to check if workers can build mountainHeight in 'mid' seconds
-    private boolean check(long mid, int[] wt, int mh) {
-
-        long totalHeight = 0;
-
-        for (int t : wt) {
-
-            // maximum layers a worker can build in 'mid' seconds
-            long height = (long)((-1 + Math.sqrt(1 + 8.0 * mid / t)) / 2);
-
-            totalHeight += height;
-
-            if (totalHeight >= mh) {
-                return true;
+        // now check
+        for (int i = 0; i < worker.length; i++) {
+            long j = 1L;
+            long curr = 0;
+            while (height > 0 && curr + j * worker[i] <= time) {
+                curr += j * worker[i];
+                j++;
+                height--;
             }
+            if (height == 0)
+                return true;
+
         }
 
-        return totalHeight >= mh;
+return height == 0 ? true : false ;
     }
 
     public long minNumberOfSeconds(int mountainHeight, int[] workerTimes) {
-
-        int min = Integer.MAX_VALUE;
-
-        for (int t : workerTimes) {
-            min = Math.min(min, t);
+        long min = 1;
+        long max = 0;
+        long dummy = 0;
+        // find max 
+        for (int ele : workerTimes) {
+            dummy = Math.max(ele, dummy);
+        }
+        // find range 
+        for (int i = 1; i <= mountainHeight; i++) {
+            max += dummy * i;
         }
 
-        long left = 1;
-        long right = (long) min * mountainHeight * (mountainHeight + 1) / 2;
-
-        long ans = right;
-
-        while (left <= right) {
-
-            long mid = left + (right - left) / 2;
-
-            if (check(mid, workerTimes, mountainHeight)) {
-                ans = mid;
-                right = mid - 1;
+        // apply binary search
+        while (max > min) {
+            // find mid 
+            long mid = (min + max) / 2;
+            if (isPossible(mid, mountainHeight, workerTimes)) {
+                max = mid;
             } else {
-                left = mid + 1;
+                min = mid + 1;
             }
-        }
 
-        return ans;
+        }
+        return min;
+
     }
 }
