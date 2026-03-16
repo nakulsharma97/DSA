ed01ff47e9b8f566c478452e9dc07b213b380fae
Nakul Sharma <nakulsharma978397@gmail.com>
2026-03-16 10:47:49 +0530

    Time: 49 ms (58.82%) | Memory: 47.6 MB (64.71%) - LeetSync

--- 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
+++ 1990-get-biggest-three-rhombus-sums-in-a-grid/get-biggest-three-rhombus-sums-in-a-grid.java
@@ -6,45 +6,41 @@ class Solution {
         int m = grid.length;
         int n = grid[0].length;
 
-        TreeSet<Integer> set = new TreeSet<>(Collections.reverseOrder());
+        TreeSet<Integer> set = new TreeSet<>();
 
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
+        for(int r = 0; r < m; r++){
+            for(int c = 0; c < n; c++){
 
                 // side = 0
                 set.add(grid[r][c]);
+                if(set.size() > 3) set.remove(set.first());
 
-                for (int side = 1; r - side >= 0 && r + side < m && c - side >= 0 && c + side < n; side++) {
+                for(int side = 1; r - side >= 0 && r + side < m && c - side >= 0 && c + side < n; side++){
 
                     int sum = 0;
 
-                    for (int k = 0; k < side; k++) {
+                    for(int k = 0; k < side; k++){
 
-                        // top -> right
-                        sum += grid[r - side + k][c + k];
-
-                        // right -> bottom
-                        sum += grid[r + k][c + side - k];
-
-                        // bottom -> left
-                        sum += grid[r + side - k][c - k];
-
-                        // left -> top
-                        sum += grid[r - k][c - side + k];
+                        sum += grid[r - side + k][c + k];      // top -> right
+                        sum += grid[r + k][c + side - k];      // right -> bottom
+                        sum += grid[r + side - k][c - k];      // bottom -> left
+                        sum += grid[r - k][c - side + k];      // left -> top
                     }
 
                     set.add(sum);
+
+                    if(set.size() > 3){
+                        set.remove(set.first());
+                    }
                 }
             }
         }
 
-        int size = Math.min(3, set.size());
-        int[] ans = new int[size];
+        int[] ans = new int[set.size()];
+        int i = ans.length - 1;
 
-        int i = 0;
-        for (int val : set) {
-            if (i == size) break;
-            ans[i++] = val;
+        for(int val : set){
+            ans[i--] = val; // reverse order
         }
 
         return ans;
