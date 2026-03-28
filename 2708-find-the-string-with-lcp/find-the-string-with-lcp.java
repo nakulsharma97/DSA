bcaa0194cb1387303316a9a4a70f550ce1f1b089
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-03-28 16:01:53 +0530

    Time: 302 ms (5.71%) | Memory: 173.1 MB (91.43%) - LeetSync

--- 2708-find-the-string-with-lcp/find-the-string-with-lcp.java
+++ 2708-find-the-string-with-lcp/find-the-string-with-lcp.java
@@ -2,39 +2,58 @@ class Solution {
     public String findTheString(int[][] lcp) {
         int n = lcp.length;
         char[] word = new char[n];
-        char current = 'a';
 
+        // Fill placeholder
         for (int i = 0; i < n; i++) {
-            if (word[i] == 0) {
-                if (current > 'z') {
-                    return "";
+            word[i] = '$';
+        }
+
+        // Step 1: Construct string
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                if (lcp[j][i] != 0) {
+                    word[i] = word[j];
+                    break;
                 }
-                word[i] = current;
-                for (int j = i + 1; j < n; j++) {
-                    if (lcp[i][j] > 0) {
-                        word[j] = word[i];
+            }
+
+            if (word[i] == '$') {
+                boolean[] forb = new boolean[26];
+
+                for (int j = 0; j < i; j++) {
+                    if (lcp[j][i] == 0) {
+                        forb[word[j] - 'a'] = true;
                     }
                 }
-                current++;
+
+                for (int idx = 0; idx < 26; idx++) {
+                    if (!forb[idx]) {
+                        word[i] = (char) (idx + 'a');
+                        break;
+                    }
+                }
+
+                if (word[i] == '$') return "";
             }
         }
 
+        // Step 2: VALIDATE LCP
         for (int i = n - 1; i >= 0; i--) {
             for (int j = n - 1; j >= 0; j--) {
-                if (word[i] != word[j]) {
-                    if (lcp[i][j] != 0) {
-                        return "";
-                    }
-                } else {
-                    if (i == n - 1 || j == n - 1) {
-                        if (lcp[i][j] != 1) {
-                            return "";
-                        }
-                    } else {
-                        if (lcp[i][j] != lcp[i + 1][j + 1] + 1) {
-                            return "";
-                        }
-                    }
+
+                int expected = lcp[i][j];
+                int actual = 0;
+
+                int x = i, y = j;
+                while (x < n && y < n && word[x] == word[y]) {
+                    actual++;
+                    x++;
+                    y++;
+                }
+
+                if (actual != expected) {
+                    return "";
                 }
             }
         }
