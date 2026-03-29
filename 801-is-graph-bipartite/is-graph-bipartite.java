50a90f5ac3d8dd813506309bbf2b44de9fd6899d
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-03-29 12:25:04 +0530

    Time: 1 ms (64.06%) | Memory: 47.4 MB (13.39%) - LeetSync

--- 801-is-graph-bipartite/is-graph-bipartite.java
+++ 801-is-graph-bipartite/is-graph-bipartite.java
@@ -1,40 +1,36 @@
 class Solution {
-    public boolean check(int start ,int[][] graph, int color[] ){
- Queue<Integer> q = new LinkedList<>() ;
-        q.add(start) ;
-        int n = graph.length ;
-        color[start] = 0  ;
-        while(!q.isEmpty()){
-            int node = q.poll() ;
-            for(int idx : graph[node]){
-                //if adjecaent node not color 
-                // give opposite 
-                if(color[idx] == -1){
-                  color[idx] = 1- color[node] ;
-                  q.add(idx) ;
-                }
-                // is adjacent guy have  same color somone did it before
-                else if(color[node] == color[idx]){
-                    return false ;
+    public boolean check(int start, int[][] graph, int[] color) {
+        int n = graph.length;
+        Queue<Integer> q = new LinkedList<>();
+        q.add(start);
+        color[start] = 0;
+        while (!q.isEmpty()) {
+            int node = q.poll();
+            for (int idx : graph[node]) {
+                if (color[idx] == -1) {
+                    color[idx] = 1 - color[node];
+                    q.add(idx);
+                } else if (color[idx] == color[node]) {
+                    return false;
                 }
             }
         }
-return true ;
+        return true;
     }
-    public boolean isBipartite(int[][] graph) {
-        int n = graph.length ;
-       int color[] = new int[n] ;
-        for(int i = 0 ; i <  n  ;i++){
-            color[i] = -1   ;
-        }
-        for(int i = 0 ;i < n ;i++){
-      if(color[i] == -1){
-        if(check(i , graph , color) == false){
 
-            return false ;
+    public boolean isBipartite(int[][] graph) {
+        int n = graph.length;
+        int color[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            color[i] = -1;
         }
-      }
+        for (int i = 0; i < n; i++) {
+            if(color[i] == -1){
+            if (check(i, graph, color) == false) {
+                return false;
+            }
+            }
         }
-        return true ;
+        return true;
     }
 }
