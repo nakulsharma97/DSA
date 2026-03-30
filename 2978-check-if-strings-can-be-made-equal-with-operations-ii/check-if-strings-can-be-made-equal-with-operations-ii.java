88d45d96d719dec439c49194751b42fe92dd9ba6
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-03-30 15:32:28 +0530

    Time: 5 ms (98.68%) | Memory: 48.1 MB (53.95%) - LeetSync

--- 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
+++ 2978-check-if-strings-can-be-made-equal-with-operations-ii/check-if-strings-can-be-made-equal-with-operations-ii.java
@@ -1,30 +1,23 @@
-import java.util.*;
-
 class Solution {
     public boolean checkStrings(String s1, String s2) {
-        int n = s1.length();
-        char[] even1 = new char[(n + 1) / 2];
-        char[] odd1 = new char[n / 2];
-        char[] even2 = new char[(n + 1) / 2];
-        char[] odd2 = new char[n / 2];
-
-        int e1 = 0, o1 = 0, e2 = 0, o2 = 0;
-
-        for (int i = 0; i < n; i++) {
-            if (i % 2 == 0) {
-                even1[e1++] = s1.charAt(i);
-                even2[e2++] = s2.charAt(i);
-            } else {
-                odd1[o1++] = s1.charAt(i);
-                odd2[o2++] = s2.charAt(i);
+        int even[] = new int[26] ;
+        int odd[] = new int[26] ;
+        int n = s1.length() ;
+        for(int i = 0 ; i < n ;i++){
+            if(i % 2 == 0){
+                even[s1.charAt(i) -'a']++ ;
+                even[s2.charAt(i) - 'a'] -- ;
+            }
+            else {
+                odd[s1.charAt(i) - 'a']++ ;
+                odd[s2.charAt(i) - 'a']-- ;
             }
         }
-
-        Arrays.sort(even1);
-        Arrays.sort(even2);
-        Arrays.sort(odd1);
-        Arrays.sort(odd2);
-
-        return Arrays.equals(even1, even2) && Arrays.equals(odd1, odd2);
+        for(int i = 0 ; i < 26 ;i++){
+            if(even[i] != 0 || odd[i] != 0){
+                return false  ;
+            }
+        }
+        return true ;
     }
 }
