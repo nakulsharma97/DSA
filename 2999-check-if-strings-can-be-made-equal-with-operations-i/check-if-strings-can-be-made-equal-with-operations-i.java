9e4a0472be3ab82aeff4419f90388a4b84d9eeb1
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-03-30 15:22:14 +0530

    Time: 1 ms (100.00%) | Memory: 44.5 MB (35.94%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.java
@@ -1,10 +1,7 @@
 class Solution {
     public boolean canBeEqual(String s1, String s2) {
-     if(s1.charAt(0)  != s2.charAt(0) && (s1.charAt(0) != s2.charAt(2) ||  s1.charAt(2)  != s2.charAt(0))) return false ;
-     if(s1.charAt(1)  != s2.charAt(1) && (s1.charAt(1) != s2.charAt(3) || s1.charAt(3) != s2.charAt(1))) return false ;
-if(s1.charAt(0)==s2.charAt(0)&&s1.charAt(2)!=s2.charAt(2))return false;
-        if(s1.charAt(1)==s2.charAt(1)&&s1.charAt(3)!=s2.charAt(3))return false;
-    
-return true ;
+     boolean condition = (s1.charAt(0) == s2.charAt(0) && s1.charAt(2) == s2.charAt(2)) || (s1.charAt(2) == s2.charAt(0) && s1.charAt(0) == s2.charAt(2))  ;
+     boolean condition1 = (s1.charAt(1) == s2.charAt(1) && s1.charAt(3) == s2.charAt(3)) || (s1.charAt(1) == s2.charAt(3) && s1.charAt(3) == s2.charAt(1)) ;
+     return condition && condition1 ;    
     }
 }
