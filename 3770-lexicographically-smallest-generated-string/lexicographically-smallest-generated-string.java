df1971bf22683d0e382e9ebaee937f467b10f8d4
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-01 10:42:46 +0530

    Time: 11 ms (78.00%) | Memory: 46.7 MB (68.00%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
@@ -1,56 +1,75 @@
+import java.util.*;
+
 class Solution {
-    public String generateString(String S, String t) {
-        char[] s = S.toCharArray();
-        int n = s.length;
-        int m = t.length();
-        char[] ans = new char[n + m - 1];
-        Arrays.fill(ans, '?'); // '?' indicates undecided positions
 
-        // Process T
-        for (int i = 0; i < n; i++) {
-            if (s[i] != 'T') {
-                continue;
+    public boolean isSame(char[] word, String str2, int i, int m) {
+        for (int j = 0; j < m; j++) {
+            if (word[i] != str2.charAt(j)) {
+                return false;
             }
-            // Substring must equal t
-            for (int j = 0; j < m; j++) {
-                char v = ans[i + j];
-                if (v != '?' && v != t.charAt(j)) {
-                    return "";
-                }
-                ans[i + j] = t.charAt(j);
-            }
+            i++;
         }
+        return true;
+    }
 
-        char[] oldAns = ans.clone();
-        for (int i = 0; i < ans.length; i++) {
-            if (ans[i] == '?') {
-                ans[i] = 'a'; // initial value for undecided positions is 'a'
-            }
-        }
+    public String generateString(String str1, String str2) {
 
-        // Process F
+        int n = str1.length();
+        int m = str2.length();
+        int N = n + m - 1;
+
+        char[] word = new char[N];
+        Arrays.fill(word, '$');
+
+        boolean[] canChange = new boolean[N];
+
+        // process T
         for (int i = 0; i < n; i++) {
-            if (s[i] != 'F') {
-                continue;
-            }
-            // Substring must not equal t
-            if (!new String(ans, i, m).equals(t)) {
-                continue;
-            }
-            // Find the last undecided position
-            boolean ok = false;
-            for (int j = i + m - 1; j >= i; j--) {
-                if (oldAns[j] == '?') { // previously filled with 'a', now change to 'b'
-                    ans[j] = 'b';
-                    ok = true;
-                    break;
+            if (str1.charAt(i) == 'T') {
+               
+
+                int i_ = i;
+                for (int j = 0; j < m; j++) {
+                    if (word[i_] != '$' && word[i_] != str2.charAt(j)) {
+                        return "";
+                    }
+                    word[i_] = str2.charAt(j);
+                    i_++;
                 }
             }
-            if (!ok) {
-                return "";
+        }
+
+        // fill remaining with 'a'
+        for (int i = 0; i < N; i++) {
+            if (word[i] == '$') {
+                word[i] = 'a';
+                canChange[i] = true;
             }
         }
 
-        return new String(ans);
+        // process F
+        for (int i = 0; i < n; i++) {
+            if (str1.charAt(i) == 'F') {
+
+                if (i + m > N) continue;  // 🔥 FIX
+
+                if (isSame(word, str2, i, m)) {
+
+                    boolean change = false;
+
+                    for (int k = i + m - 1; k >= i; k--) {
+                        if (canChange[k]) {
+                            word[k] = 'b';
+                            change = true;
+                            break;
+                        }
+                    }
+
+                    if (!change) return "";
+                }
+            }
+        }
+
+        return new String(word);
     }
 }
