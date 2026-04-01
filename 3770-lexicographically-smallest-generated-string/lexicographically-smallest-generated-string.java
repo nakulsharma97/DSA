8453b9e0d99c5853c98790713f8c41d85f7335e3
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-01 11:06:51 +0530

    Time: 12 ms (56.00%) | Memory: 46.5 MB (84.00%) - LeetSync

--- 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
+++ 3770-lexicographically-smallest-generated-string/lexicographically-smallest-generated-string.java
@@ -1,73 +1,54 @@
-import java.util.*;
-
 class Solution {
+    public boolean isSame(char[] word , String str2 , int i , int m){
+        for(int j = 0 ; j < m ;j++){
+            if(word[i] != str2.charAt(j)){
 
-    public boolean isSame(char[] word, String str2, int i, int m) {
-        for (int j = 0; j < m; j++) {
-            if (word[i] != str2.charAt(j)) {
-                return false;
+                return false ;
             }
-            i++;
+            i++ ;
         }
-        return true;
+        return true ;
     }
-
     public String generateString(String str1, String str2) {
-
-        int n = str1.length();
-        int m = str2.length();
-        int N = n + m - 1;
-
-        char[] word = new char[N];
-        Arrays.fill(word, '$');
-
-        boolean[] canChange = new boolean[N];
-
-        // process T
-        for (int i = 0; i < n; i++) {
-            if (str1.charAt(i) == 'T') {
-                if (i + m > N) return "";  // 🔥 FIX
-                int i_ = i;
-                for (int j = 0; j < m; j++) {
-                    if (word[i_] != '$' && word[i_] != str2.charAt(j)) {
-                        return "";
+        int n = str1.length()  ;
+        int m = str2.length() ;
+        int N = n + m -1 ;
+        boolean[] canChange = new boolean[N] ;
+        char[] word = new char[N] ;
+        Arrays.fill(word , '$') ;
+        for(int i =  0 ; i < n ;i++){
+            if(str1.charAt(i) == 'T'){
+                int i_ = i ;
+                for(int j = 0 ; j < m ;j++){
+                    if(word[i_] != '$' && word[i_] != str2.charAt(j)){
+                        return "" ;
                     }
-                    word[i_] = str2.charAt(j);
-                    i_++;
+                    word[i_] = str2.charAt(j) ;
+                    i_++ ;
                 }
             }
         }
-
-        // fill remaining with 'a'
-        for (int i = 0; i < N; i++) {
-            if (word[i] == '$') {
-                word[i] = 'a';
-                canChange[i] = true;
+        for(int i = 0 ; i < N ;i++){
+            if(word[i] == '$'){
+                word[i] = 'a' ;
+                canChange[i] =true ;
             }
         }
-
-        // process F
-        for (int i = 0; i < n; i++) {
-            if (str1.charAt(i) == 'F') {
-
-
-                if (isSame(word, str2, i, m)) {
-
-                    boolean change = false;
-
-                    for (int k = i + m - 1; k >= i; k--) {
-                        if (canChange[k]) {
-                            word[k] = 'b';
-                            change = true;
-                            break;
+        for(int i = 0 ; i < n ;i++){
+            if(str1.charAt(i) == 'F'){
+                if(isSame(word , str2 , i , m)){
+                    boolean change = false ;
+                    for(int k = i + m - 1 ; k >= i ;k--){
+                        if(canChange[k]){
+                            word[k] ='b' ;
+                            change = true ;
+                            break ;
                         }
                     }
-
-                    if (!change) return "";
+                    if(!change) return "" ;
                 }
             }
         }
-
         return new String(word);
     }
 }
