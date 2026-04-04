0d38cc76c186e4becdb316d22851b1a9d8ec33e7
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-04 15:17:19 +0530

    Time: 11 ms (22.65%) | Memory: 47 MB (84.41%) - LeetSync

--- 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
+++ 329-longest-increasing-path-in-a-matrix/longest-increasing-path-in-a-matrix.java
@@ -1,29 +1,40 @@
 class Solution {
     public int longestIncreasingPath(int[][] matrix) {
-int n=matrix.length,m=matrix[0].length;
-        int[][]visited=new int[n][m];
-        for(int[]row:visited) Arrays.fill(row,-1);
-        int max=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                max=Math.max(max,dfs(matrix,visited,i,j,n,m));
+        int m = matrix.length ;
+        int n = matrix[0].length ;
+        int visited[][]  = new int[m][n] ;
+        for(int row[] : visited) {
+            Arrays.fill(row , -1) ;
+        }
+        int max = 0  ;
+        for(int i = 0 ; i < m ;i++){
+            for(int j = 0  ; j < n; j++){
+                max = Math.max(max , dfs(matrix , visited  , i ,j ,m, n)) ;
             }
         }
-        return max;
-    }
-    public static int dfs(int[][]arr,int[][]visited,int row,int col,int n,int m){
-        if(visited[row][col]!=-1)return visited[row][col];
-        int[]allowedRow={0,-1,1,0};
-        int[]allowedCol={-1,0,0,1};
-        int max=1;
-        for(int i=0;i<4;i++){
-            int newRow=row+allowedRow[i];
-            int newCol=col+allowedCol[i];
-            if(newRow>=0&&newRow<n&&newCol>=0&&newCol<m&&arr[row][col]<arr[newRow][newCol]){
-               max=Math.max(max,1+dfs(arr,visited,newRow,newCol,n,m));
+        return max ;
+
+    } 
+    public int dfs(int[][]matrix , int[][] visited , int row , int col , int m , int n){
+        if(visited[row][col] != -1) return visited[row][col] ;
+        int[][] dir = {
+            {0 , 1 },
+            {1 , 0},
+            {-1 , 0},
+            {0 , -1} 
+          } ;
+           int max = 1 ;
+          for(int i = 0 ; i<4 ;i++){
+            int newrow = row + dir[i][0] ;
+            int newcol = col + dir[i][1] ;
+           
+            if(newrow >= 0 && newrow < m && newcol >= 0 && newcol < n && matrix[newrow][newcol] > matrix[row][col])
+            {
+                max = Math.max( max , 1 + dfs(matrix , visited , newrow , newcol , m , n )) ;
             }
-        }
-        visited[row][col]=max;
-        return max;
+          }
+          visited[row][col] = max ;
+          return max ;
+
     }
 }
