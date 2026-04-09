c15d9d4aca03b5bddff9dfe2eabd79f1794a1a93
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-09 16:03:08 +0530

    Time: 6 ms (97.22%) | Memory: 113 MB (91.65%) - LeetSync

--- 2465-shifting-letters-ii/shifting-letters-ii.java
+++ 2465-shifting-letters-ii/shifting-letters-ii.java
@@ -1,32 +1,31 @@
 class Solution {
     public String shiftingLetters(String s, int[][] shifts) {
         int n = s.length();
-        int[] diff = new int[n];
-
-        for (int[] ad : shifts) {
+        int diff[] = new int[n];
+        for (int ad[] : shifts) {
             int start = ad[0];
             int end = ad[1];
-            int val = (ad[2] == 1) ? 1 : -1;
-            diff[start] += val;
+            int x = (ad[2] == 1) ? 1 : -1;
+
+            diff[start] += x;
             if (end + 1 < n) {
-                diff[end + 1] -= val;
+                diff[end + 1] -= x;
             }
+
         }
-
-        // prefix sum
-        for (int i = 1; i < n; i++) {
-            diff[i] += diff[i - 1];
+        for(int i = 1 ; i < n ;i++){
+            diff[i]  = diff[i]  + diff[i-1] ;
         }
+        char cc[]  = new char[n] ;
+        for(int i = 0  ;i < n ;i++){
+            int shift = diff[i] % 26 ;
+            if(shift < 0){
+                shift = shift + 26 ;
+            }
+            int ab = s.charAt(i) - 'a' ;
+            cc[i] = (char)((ab + shift) % 26 + 'a') ;
 
-        char[] cc = new char[n];
-
-        for (int i = 0; i < n; i++) {
-            int shift = diff[i] % 26;
-            if (shift < 0) shift += 26;
-            int ab = s.charAt(i) - 'a';
-            cc[i] = (char) ((ab + shift) % 26 + 'a');
         }
-
-        return new String(cc);
+        return new String(cc) ;
     }
 }
