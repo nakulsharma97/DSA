500b93177191f748c6fad7ec9fa4c6decec333d7
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-11 13:13:18 +0530

    Time: 104 ms (77.65%) | Memory: 273.8 MB (54.19%) - LeetSync

--- 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
+++ 4119-minimum-distance-between-three-equal-elements-ii/minimum-distance-between-three-equal-elements-ii.java
@@ -1,26 +1,26 @@
 class Solution {
     public int minimumDistance(int[] nums) {
-            HashMap<Integer ,List<Integer>> map = new HashMap<>() ;
-            int n = nums.length ;
-            int res = Integer.MAX_VALUE ;
-            for(int i = 0 ;i < n ;i++){
-                int num = nums[i] ;
-                List<Integer> list = map.getOrDefault(num , new ArrayList<>()) ;
-                if(list.size() == 2){
-                    int val1  = list.get(0) ;
-                    int val2 = list.get(1) ;
-                    int val3 = i ;
-                    int dist = Math.abs(val1 - val2) + Math.abs(val2 - val3) + Math.abs(val3 - val1) ;
-res = Math.min(res , dist) ;
-list.set(0 , val2)  ;
-list.set(1 , val3) ;
+        HashMap<Integer, List<Integer>> map = new HashMap<>() ;
+        int n = nums.length ;
+        int res = Integer.MAX_VALUE ;
+        for(int i =0 ;i < n;i++){
+            int num =nums[i] ;
+            List<Integer> list = map.getOrDefault(num , new ArrayList<>()) ;
+            if(list.size()  == 2){
+                int val1 = list.get(0) ;
+                int val2 = list.get(1) ;
+                int val3 = i  ;
+                int dist = Math.abs(val1 - val2) + Math.abs(val2 - val3) + Math.abs(val3 - val1) ;
+                res =Math.min(dist ,res) ;
+                list.set(0 , val2) ;
+                list.set(1 , val3) ;
 
-                }
-                else {
-                    list.add(i);
-                }
-                map.put(num , list) ;
             }
-           return res!=Integer.MAX_VALUE?res:-1;
+            else {
+                list.add(i) ;
+            }
+            map.put(num , list) ;
+        }
+        return (res == Integer.MAX_VALUE)? -1 : res ;
     }
 }
