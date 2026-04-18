c83f660605b1a4f56d4e5f2acf51a15b224330d7
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-18 11:28:05 +0530

    Time: 108 ms (56.81%) | Memory: 182.2 MB (91.18%) - LeetSync

--- 3750-closest-equal-element-queries/closest-equal-element-queries.java
+++ 3750-closest-equal-element-queries/closest-equal-element-queries.java
@@ -1,49 +1,35 @@
-import java.util.*;
-
 class Solution {
     public List<Integer> solveQueries(int[] nums, int[] queries) {
-        int n = nums.length;
-
-        // Step 1: store indices of each number
-        Map<Integer, List<Integer>> map = new HashMap<>();
-
-        for (int i = 0; i < n; i++) {
-            if (!map.containsKey(nums[i])) {
-                map.put(nums[i], new ArrayList<>());
+        HashMap<Integer , List<Integer>> map = new HashMap<>() ;
+        int n = nums.length ;
+        for(int i = 0 ;i < n ;i++){
+            if(!map.containsKey(nums[i])){
+                map.put(nums[i] , new ArrayList<>()) ;
             }
-            map.get(nums[i]).add(i);
+                map.get(nums[i]).add(i) ;
+            
         }
-
-        List<Integer> ans = new ArrayList<>();
-
-        // Step 2: process queries
-        for (int q : queries) {
-            List<Integer> v = map.get(nums[q]);
-
-            // if only one occurrence
-            if (v.size() == 1) {
-                ans.add(-1);
-                continue;
-            }
-
-            // binary search to find position of q
-            int pos = Collections.binarySearch(v, q);
+        List<Integer> ans = new ArrayList<>() ;
+        for(int q : queries){
+            List<Integer> list = map.get(nums[q]) ;
+             if(list.size() == 1){
+                ans.add(-1) ;
+                continue ;
+             }
+           int pos = Collections.binarySearch(list, q);
 
             int res = Integer.MAX_VALUE;
 
-            // left neighbor (circular)
-            int left = v.get((pos - 1 + v.size()) % v.size());
-            int d1 = Math.abs(q - left);
-            res = Math.min(res, Math.min(d1, n - d1));
-
-            // right neighbor (circular)
-            int right = v.get((pos + 1) % v.size());
-            int d2 = Math.abs(q - right);
-            res = Math.min(res, Math.min(d2, n - d2));
-
-            ans.add(res);
+     int right = list.get((pos + 1) % list.size()) ;
+     int d = Math.abs(right - q) ;
+     res = Math.min(res , Math.min( d , n - d)) ;
+      
+int left = list.get((pos - 1 + list.size()) % list.size());
+int d1 = Math.abs(left - q) ;
+res = Math.min( res , Math.min(d1 , n - d1)) ;
+ 
+ans.add(res) ;
         }
-
-        return ans;
+        return ans ;
     }
 }
