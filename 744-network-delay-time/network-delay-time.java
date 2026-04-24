58b8952bd63fa601e71a8677f16ec80588c3f548
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-24 12:39:08 +0530

    Time: 11 ms (66.93%) | Memory: 49.1 MB (55.79%) - LeetSync

--- 744-network-delay-time/network-delay-time.java
+++ 744-network-delay-time/network-delay-time.java
@@ -1,52 +1,44 @@
 class Solution {
     public int networkDelayTime(int[][] times, int n, int k) {
-
-       
-        List<List<int[]>> adj = new ArrayList<>();
-        for (int i = 0; i <= n; i++) {
-            adj.add(new ArrayList<>());
+        List<List<int[]>> adj = new ArrayList<>() ;
+        for(int i = 0 ; i <= n ;i++){
+            adj.add(new ArrayList<>()) ;
         }
-
-        for (int[] t : times) {
-            adj.get(t[0]).add(new int[]{t[1], t[2]});
+        for(int t[]  : times){
+            adj.get(t[0]).add(new int[] {t[1] , t[2]}) ;
         }
-         // (node , time)
-        PriorityQueue<int[]> pq = new PriorityQueue<>(
-            (a, b) -> a[1] - b[1]
-        );
+        int[] mint = new int[n + 1] ;
+        Arrays.fill(mint , Integer.MAX_VALUE) ;
+        mint[k] = 0 ;
+        PriorityQueue<int[] > pq = new PriorityQueue<>(
+            (a , b) -> a[1] - b[1]
+        )  ;
+        pq.offer(new int [] {k , 0}) ;
+        while(!pq.isEmpty()){
+            int size = pq.size() ;
+            while(size-- > 0){
+                int curr[] = pq.poll() ;
+                for(int neigh[] : adj.get(curr[0])) {
+                       int next = neigh[0] ;
+                       int time = neigh[1] ;
+                       if( curr[1] + time >= mint[next]){
+                        continue ;
+                       }
+                       mint[next] = curr[1] + time ;
+                       pq.offer(new int[]{next , curr[1] + time}) ;
 
-        // Step 3: Distance array
-        int[] dist = new int[n + 1];
-        Arrays.fill(dist, Integer.MAX_VALUE);
-
-        dist[k] = 0;
-        pq.offer(new int[]{k, 0});
-
-        while (!pq.isEmpty()) {
-            int[] curr = pq.poll();
-            int node = curr[0];
-            int time = curr[1];
-
-            if (time > dist[node]) continue;
-
-            for (int[] nei : adj.get(node)) {
-                int next = nei[0];
-                int wt = nei[1];
-
-                if (time + wt < dist[next]) {
-                    dist[next] = time + wt;
-                    pq.offer(new int[]{next, dist[next]});
                 }
+
             }
         }
-
-        // Step 5: Find result
-        int res = 0;
-        for (int i = 1; i <= n; i++) {
-            if (dist[i] == Integer.MAX_VALUE) return -1;
-            res = Math.max(res, dist[i]);
+        int res = 0 ;
+        for(int i = 1 ; i <= n ;i++){
+            if(mint[i] == Integer.MAX_VALUE){
+                return -1 ;
+            }
+            res = Math.max(res , mint[i]) ;
         }
-
-        return res;
+        return res ;
     }
+
 }
