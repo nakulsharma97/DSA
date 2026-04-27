cdebe49423bf17a7ba72a31d914ed4a280750644
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-04-27 18:10:30 +0530

    Time: 19 ms (67.83%) | Memory: 102.7 MB (33.04%) - LeetSync

--- 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
+++ 1507-check-if-there-is-a-valid-path-in-a-grid/check-if-there-is-a-valid-path-in-a-grid.java
@@ -1,40 +1,50 @@
 class Solution {
-    private static final int[][] TRANS = {
-        {-1, 1, -1, 3},
-        {0, -1, 2, -1},
-        {3, 2, -1, -1},
-        {1, -1, -1, 2},
-        {-1, 0, 3, -1},
-        {-1, -1, 1, 0}
-    };
-    private static final int[][] DIRS = {{-1, 0}, {0, 1}, {1, 0}, {0, -1}};
-    private static final int[][] START = {{1, 3}, {0, 2}, {2, 3},
-                                          {1, 2}, {0, 3}, {0, 1}};
 
+    // directions: up, right, down, left
+    int[][] dirs = {{-1,0},{0,1},{1,0},{0,-1}};
+    
     public boolean hasValidPath(int[][] grid) {
-        if (grid[0][0] == 5) return false;
-        if (grid[grid.length - 1][grid[0].length - 1] == 4) return false;
-
         int m = grid.length, n = grid[0].length;
-        if (m == 1 && n == 1) return true;
-
-        int[] s = START[grid[0][0] - 1];
-        return check(grid, s[0]) || check(grid, s[1]);
+        boolean[][] vis = new boolean[m][n];
+        return dfs(grid, 0, 0, vis);
     }
 
-    private boolean check(int[][] grid, int di) {
-        if (di == -1) return false;
+    private boolean dfs(int[][] grid, int r, int c, boolean[][] vis) {
         int m = grid.length, n = grid[0].length;
-        int r = DIRS[di][0];
-        int c = DIRS[di][1];
+        
+        if (r == m - 1 && c == n - 1) return true;
+        
+        vis[r][c] = true;
 
-        while (r >= 0 && r < m && c >= 0 && c < n) {
-            di = TRANS[grid[r][c] - 1][di];
-            if (di == -1 || r == 0 && c == 0) return false;
-            if (r == m - 1 && c == n - 1) return true;
+        for (int d : getDirections(grid[r][c])) {
+            int nr = r + dirs[d][0];
+            int nc = c + dirs[d][1];
 
-            r += DIRS[di][0];
-            c += DIRS[di][1];
+            if (nr < 0 || nc < 0 || nr >= m || nc >= n || vis[nr][nc]) continue;
+
+            // check reverse connection
+            if (isConnected(grid[nr][nc], (d + 2) % 4)) {
+                if (dfs(grid, nr, nc, vis)) return true;
+            }
+        }
+        return false;
+    }
+
+    private int[] getDirections(int type) {
+        switch (type) {
+            case 1: return new int[]{1, 3}; // right, left
+            case 2: return new int[]{0, 2}; // up, down
+            case 3: return new int[]{3, 2}; // left, down
+            case 4: return new int[]{1, 2}; // right, down
+            case 5: return new int[]{3, 0}; // left, up
+            case 6: return new int[]{1, 0}; // right, up
+        }
+        return new int[]{};
+    }
+
+    private boolean isConnected(int type, int dir) {
+        for (int d : getDirections(type)) {
+            if (d == dir) return true;
         }
         return false;
     }
