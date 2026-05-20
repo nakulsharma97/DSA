835e5b982f8c5931f06b618d9059d3ed18b59c41
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-05-20 11:57:20 +0530

    Time: 5 ms (46.74%) | Memory: 46.9 MB (98.20%) - LeetSync

--- 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
+++ 2766-find-the-prefix-common-array-of-two-arrays/find-the-prefix-common-array-of-two-arrays.java
@@ -1,38 +1,36 @@
+import java.util.*;
+
 class Solution {
     public int[] findThePrefixCommonArray(int[] A, int[] B) {
-        int n = A.length;
-        int res[] = new int[n];
-         HashSet<Integer> set = new HashSet<>();
-        if (A[0] != B[0]) {
-            res[0] = 0;
-            set.add(A[0]) ;
-            set.add(B[0]);
-        }
-        if (A[0] == B[0]) {
-            res[0] = 1;
-        }
-       
-        for (int i = 1; i < n; i++) {
-            if (A[i] == B[i]) {
-                res[i] = res[i - 1] + 1;
-            } 
-            else if (A[i] != B[i]) {
-                int a = 0;
-                if (set.contains(A[i])) {
-                    a++;
-                } 
-                if (set.contains(B[i])) {
-                    a++;                                        
-                }
-                 if (!set.contains(A[i])) {
-                    set.add(A[i]);
-                } if (!set.contains(B[i])) {
-                    set.add(B[i]);
-                }
-                 
-                res[i] = res[i - 1] + a;
 
+        int n = A.length;
+        int[] res = new int[n];
+
+        HashSet<Integer> seenA = new HashSet<>();
+        HashSet<Integer> seenB = new HashSet<>();
+
+        int common = 0;
+
+        for (int i = 0; i < n; i++) {
+
+            if (seenB.contains(A[i])) {
+                common++;
             }
 
-        }return res;
-}}
+            if (seenA.contains(B[i])) {
+                common++;
+            }
+
+            if (A[i] == B[i]) {
+                common++;
+            }
+
+            seenA.add(A[i]);
+            seenB.add(B[i]);
+
+            res[i] = common;
+        }
+
+        return res;
+    }
+}
