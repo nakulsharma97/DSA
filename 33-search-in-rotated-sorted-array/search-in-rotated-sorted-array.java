bcbd9f597740c9abc249d5476e96d719ed238be3
Nakul Sharma <196036849+nakulsharma97@users.noreply.github.com>
2026-05-22 11:31:10 +0530

    Time: 0 ms (100.00%) | Memory: 43.8 MB (45.24%) - LeetSync

--- 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -1,11 +1,53 @@
 class Solution {
+    public int  pivot(int [] arr){
+        int n = arr.length ;
+        int left = 0 ;
+        int right = n-1 ;
+        int mid = 0 ;
+        while(left < right){
+             mid = left + (right - left) / 2 ;
+            if(arr[mid] > arr[right]){
+                left = mid + 1 ;
+            }
+            else {
+                right = mid ;
+            }
+            
+        }
+        return left ;
+    }
     public int search(int[] nums, int target) {
         int n = nums.length ;
-        for(int i = 0 ;i < n ;i++){
-            if(nums[i] == target){
-                return i ; 
+        int pivott = pivot(nums) ;
+        int left  = 0 ;
+        int right = pivott -1 ;
+        while(left <= right){
+            int mid = left + (right - left)  / 2 ;
+            if(nums[mid] == target){
+                return mid ;
+            }
+            if(nums[mid] > target){
+                right = mid -1  ;
+            }
+            else{
+                left = mid + 1 ;
             }
         }
+        int r = n -1 ;
+        while( pivott <= r){
+            int m = pivott + (r - pivott) / 2 ;
+            if(nums[m] == target){
+                return m ;
+            }
+            if(nums[m] > target){
+                r = m -  1 ;
+            }
+            else {
+                pivott = m + 1;
+            }
+
+        }
         return -1 ;
+
     }
 }
