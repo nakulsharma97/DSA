30cb531678a45b94585e437f1dce875b4a372387
Nakul Sharma <nakulsharma978397@gmail.com>
2026-06-13 18:31:14 +0530

    Time: 42 ms (97.48%) | Memory: 130.8 MB (79.55%) - LeetSync

---
+++ 146-lru-cache/lru-cache.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+class LRUCache {
+
+    private final int capacity;
+    private final LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        this.cache = new LinkedHashMap<>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry<Integer, Integer> eldest) {
+                return size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
