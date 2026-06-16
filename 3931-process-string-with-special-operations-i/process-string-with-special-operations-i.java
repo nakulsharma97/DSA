476b3d4140addeec0479f806f18ffefb702f223e
Nakul Sharma <nakulsharma978397@gmail.com>
2026-06-16 08:26:11 +0530

    Time: 4 ms (76.47%) | Memory: 54.2 MB (94.85%) - LeetSync

---
+++ 3931-process-string-with-special-operations-i/process-string-with-special-operations-i.java
@@ -0,0 +1,28 @@
+class Solution {
+    public String processStr(String s) {
+        StringBuilder sb = new StringBuilder() ;
+        int  n = s.length() ;
+        
+        String result = "";
+        for(int i = 0 ; i< n ;i++){ 
+            char ch = s.charAt(i) ;
+
+            if(ch == '*'){
+
+                if(sb.length() !=0){
+                    sb.deleteCharAt(sb.length() -1) ;
+                }
+            }
+            if(ch == '#'){
+                sb.append(sb) ;
+            }
+            else if(ch == '%'){
+                sb.reverse() ;
+            }
+            else if(ch >= 'a' && ch <= 'z'){
+                sb.append(ch) ;
+            }
+        }
+        return sb.toString() ;
+    }
+}
