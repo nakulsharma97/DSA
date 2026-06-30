396de686444b6e3511b470693f166fc29acc56bd
Nakul Sharma <nakulsharma978397@gmail.com>
2026-06-30 17:46:10 +0530

    Time: 1 ms (9.94%) | Memory: 43.1 MB (98.11%) - LeetSync

--- 101-symmetric-tree/symmetric-tree.java
+++ 101-symmetric-tree/symmetric-tree.java
@@ -15,21 +15,29 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-         if(root == null) return true  ;
-      Queue<TreeNode> q = new LinkedList<>() ;
-      q.offer(root.left) ;
-      q.offer(root.right) ;
-      while(!q.isEmpty()){
-       TreeNode node = q.poll() ;
-       TreeNode node1 = q.poll() ;
-       if(node == null && node1 == null) continue  ;
-       if(node == null || node1 == null) return false ;
-        if(node.val != node1.val) return false  ;
-        q.offer(node.left) ;
-        q.offer(node1.right) ;
-        q.offer(node.right) ;
-        q.offer(node1.left) ;
-      }  
-      return true ;
+        Queue<TreeNode> q = new LinkedList<>() ;
+        if(root == null){
+            return false  ;
+        }
+        q.offer(root.right) ;
+        q.offer(root.left) ;
+        while(!q.isEmpty()){
+            TreeNode node = q.poll() ;
+            TreeNode node1 = q.poll() ;
+            if(node == null && node1 == null){
+                continue  ;
+            }
+            if(node == null || node1 == null ){
+                return false  ;
+            }
+            if(node.val != node1.val){
+                return false  ;
+            }
+            q.offer(node.left) ;
+            q.offer(node1.right) ;
+            q.offer(node.right) ;
+            q.offer(node1.left) ;
+        }
+        return true  ;
     }
 }
