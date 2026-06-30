7ad6602e5647f40ff21509b529364436cdae02b3
Nakul Sharma <nakulsharma978397@gmail.com>
2026-06-30 14:40:14 +0530

    Time: 11 ms (91.60%) | Memory: 46.5 MB (30.71%) - LeetSync

--- 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
+++ 1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.java
@@ -1,28 +1,26 @@
 class Solution {
     public int numberOfSubstrings(String s) {
-        int n = s.length();
-        int a = 0, b = 0, c = 0;
-        int l = 0;
-        int ans = 0;
+        int n = s.length() ;
+        int ans = 0 ;
+        int a = 0 , b =0 ,c =0 ;
+        int l = 0 ;
+        for(int i = 0 ; i < n ;i++){
+            if (s.charAt(i) == 'a') a++ ;
+            else if (s.charAt(i) == 'b') b++ ;
+            else c++ ;
+            while(a > 0 && b > 0 && c > 0){
+                ans += (n - i) ;
+                if(s.charAt(l) == 'a') a-- ;
+                else if(s.charAt(l) == 'b') b-- ;
+                else {
+                    c-- ;
+                }
+                l++ ;
 
-        for (int r = 0; r < n; r++) {
 
-            if (s.charAt(r) == 'a') a++;
-            else if (s.charAt(r) == 'b') b++;
-            else c++;
-
-            while (a > 0 && b > 0 && c > 0) {
-
-                ans += (n - r);
-
-                if (s.charAt(l) == 'a') a--;
-                else if (s.charAt(l) == 'b') b--;
-                else c--;
-
-                l++;
             }
         }
-
-        return ans;
+        
+        return ans  ;
     }
 }
