ef8456b25992f515d36de47a6f3c231b6de792cf
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-09 12:51:07 +0530

    Time: 10 ms (44.55%) | Memory: 163.2 MB (59.41%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,15 +1,41 @@
 class Solution {
+    int[] parent;
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        int m = queries.length ;
-        boolean ans[] = new boolean[m] ;
-        int root[] = new int[n] ;
-        root[0]= 0 ;
-        for(int i = 1 ; i < n ;i++){
-           root[i] = ( nums[i] - nums[i - 1] <= maxDiff) ? root[i - 1] : i;
+        parent = new int[n];
+        // Every node is its own parent
+        for (int i = 0; i < n; i++) {
+            parent[i] = i;
         }
-        for(int i = 0 ; i < m ;i++){
-            ans[i] = root[queries[i][0]] == root[queries[i][1]];
+        // Connect adjacent nodes
+        for (int i = 1; i < n; i++) {
+            if (nums[i] - nums[i - 1] <= maxDiff) {
+                union(i - 1, i);
+            }
         }
-        return ans ;
+        boolean[] ans = new boolean[queries.length];
+        for (int i = 0; i < queries.length; i++) {
+            int u = queries[i][0];
+            int v = queries[i][1];
+
+            ans[i] = find(u) == find(v);
+        }
+
+        return ans;
+    }
+    int find(int x) {
+        if (parent[x] != x) {
+            parent[x] = find(parent[x]);   // Path Compression
+        }
+
+        return parent[x];
+    }
+    void union(int x, int y) {
+
+        int px = find(x);
+        int py = find(y);
+
+        if (px != py) {
+            parent[py] = px;
+        }
     }
 }
