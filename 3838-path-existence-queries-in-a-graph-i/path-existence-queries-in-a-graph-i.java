99c80d03b9c9fc30dbb9751cba88b4b3975eec5c
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-09 13:06:58 +0530

    Time: 9 ms (52.48%) | Memory: 163.1 MB (68.32%) - LeetSync

--- 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
+++ 3838-path-existence-queries-in-a-graph-i/path-existence-queries-in-a-graph-i.java
@@ -1,41 +1,36 @@
 class Solution {
-    int[] parent;
+    int parent[] ;
     public boolean[] pathExistenceQueries(int n, int[] nums, int maxDiff, int[][] queries) {
-        parent = new int[n];
-        // Every node is its own parent
-        for (int i = 0; i < n; i++) {
-            parent[i] = i;
+        parent = new int[n] ;
+        for(int i = 0 ; i< n;i++){
+            parent[i] = i ;
         }
-        // Connect adjacent nodes
-        for (int i = 1; i < n; i++) {
-            if (nums[i] - nums[i - 1] <= maxDiff) {
-                union(i - 1, i);
+        for(int i = 1 ; i < n ;i++){
+            if(nums[i] - nums[i-1] <= maxDiff){
+               union(i-1 , i);
             }
         }
-        boolean[] ans = new boolean[queries.length];
-        for (int i = 0; i < queries.length; i++) {
-            int u = queries[i][0];
-            int v = queries[i][1];
-
-            ans[i] = find(u) == find(v);
+        boolean ans[] = new boolean[queries.length] ;
+        for(int i  = 0 ;i < queries.length ;i++){
+       int u  = queries[i][0];
+       int v = queries[i][1] ;
+       ans[i] = find(u) == find(v) ;
         }
-
-        return ans;
+        return ans ;
     }
-    int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]);   // Path Compression
+    public int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]) ;
         }
-
-        return parent[x];
+        return parent[x] ;
     }
-    void union(int x, int y) {
+    public void union(int x , int y){
 
-        int px = find(x);
-        int py = find(y);
-
-        if (px != py) {
-            parent[py] = px;
+        int px = find(x) ;
+        int  py = find(y) ;
+        if(px != py){
+            parent[py] = px ;
         }
+
     }
 }
