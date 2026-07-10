828b8072814331f01b36b8658a3eef40f4a92c52
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-10 10:21:48 +0530

    Time: 1 ms (97.41%) | Memory: 44 MB (29.53%) - LeetSync

--- 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
+++ 1032-satisfiability-of-equality-equations/satisfiability-of-equality-equations.java
@@ -1,56 +1,45 @@
 class Solution {
-
-    int[] parent;
-    int[] size;
-
-    public int find(int x) {
-        if (parent[x] != x) {
-            parent[x] = find(parent[x]); // Path Compression
+    int [] parent ;
+    int [] size ;
+    public int find(int x){
+        if(parent[x] != x){
+            parent[x] = find(parent[x]);
         }
-        return parent[x];
+        return parent[x] ;
     }
-
-    public void union(int x, int y) {
-        int px = find(x);
-        int py = find(y);
-
-        if (px == py) return;
-
-        if (size[px] >= size[py]) {
-            parent[py] = px;
-            size[px] += size[py];
-        } else {
-            parent[px] = py;
-            size[py] += size[px];
-        }
-    }
-
-    public boolean equationsPossible(String[] equations) {
-
-        parent = new int[26];
-        size = new int[26];
-
-        for (int i = 0; i < 26; i++) {
-            parent[i] = i;
-            size[i] = 1;
-        }
-
-        // Process all equalities
-        for (String s : equations) {
-            if (s.charAt(1) == '=') {
-                union(s.charAt(0) - 'a', s.charAt(3) - 'a');
+    public void  union(int x , int y){
+        int px = find(x) ;
+        int py = find(y)  ;
+        if(px != py){
+            if(size[px] < size[py]){
+                parent[px] = py ;
+                size[py] += size[px] ;
+            }
+            else {
+                parent[py] =  px  ;
+                size[px] += size[px] ;
             }
         }
-
-        // Check inequalities
-        for (String s : equations) {
-            if (s.charAt(1) == '!') {
-                if (find(s.charAt(0) - 'a') == find(s.charAt(3) - 'a')) {
-                    return false;
+    }
+       public boolean equationsPossible(String[] equations) {
+        parent = new int[26] ;
+        size = new int[26] ;
+        for(int i = 0 ; i < 26 ;i++){
+            parent[i] = i ;
+            size[i] = 1 ;
+        }
+        for(String s : equations){
+            if(s.charAt(1) == '='){
+                union(s.charAt(0) - 'a' , s.charAt(3) - 'a') ;
+            }
+        }
+        for(String s : equations){
+            if(s.charAt(1) == '!'){
+                if(find(s.charAt(0) - 'a') == find(s.charAt(3) - 'a')){
+                    return false ;
                 }
             }
         }
-
-        return true;
+        return true  ;
     }
 }
