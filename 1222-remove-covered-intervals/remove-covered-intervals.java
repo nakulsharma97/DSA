a84db80e4cbfb7027330ef62264942f450b20c9a
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-21 12:35:30 +0530

    Time: 6 ms (92.01%) | Memory: 46.7 MB (8.44%) - LeetSync

--- 1222-remove-covered-intervals/remove-covered-intervals.java
+++ 1222-remove-covered-intervals/remove-covered-intervals.java
@@ -1,14 +1,14 @@
 class Solution {
     public int removeCoveredIntervals(int[][] intervals) {
-     Arrays.sort(intervals , (a , b) -> a[0] == b[0] ? b[1] - a[1] : a[0] - b[0]) ;
-     int count = 0;
-     int max =  0 ;
-     for(int interval[] : intervals) {
-        if(interval[1] > max){
+     Arrays.sort(intervals , (a , b) -> (a[0] == b[0]) ? b[1] - a[1]  : a[0] - b[0])   ;
+     int count =0 ;
+     int max  =  0;
+     for(int it[]  : intervals){
+        if(it[1] > max){
+            max = it[1] ;
             count++ ;
-            max  = interval[1] ;
         }
-     }  
+     } 
      return count ;
     }
 }
