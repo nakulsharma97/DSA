bfc84a522bf68961ebf3e980234f167eb22ad03b
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-24 17:13:01 +0530

    Time: 28 ms (73.91%) | Memory: 104.3 MB (68.75%) - LeetSync

--- 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
+++ 2022-maximum-alternating-subsequence-sum/maximum-alternating-subsequence-sum.java
@@ -1,39 +1,14 @@
 class Solution {
-
-    int n;
-    long[][] t;
-
-    public long solve(int idx, int[] nums, boolean iseven) {
-
-        if (idx >= n) {
-            return 0;
-        }
-
-        if (t[idx][iseven ? 1 : 0] != -1) {
-            return t[idx][iseven ? 1 : 0];
-        }
-
-        long skip = solve(idx + 1, nums, iseven);
-
-        long val = nums[idx];
-        if (iseven == false) {
-            val = -val;
-        }
-
-        long take = solve(idx + 1, nums, !iseven) + val;
-
-        return t[idx][iseven ? 1 : 0] = Math.max(skip, take);
-    }
-
     public long maxAlternatingSum(int[] nums) {
-
-        n = nums.length;
-        t = new long[n + 1][2];
-
-        for (int i = 0; i <= n; i++) {
-            Arrays.fill(t[i], -1);
+        int n = nums.length ;
+        long t[][] = new long[n+1][2] ;
+        for(long row[] : t){
+            Arrays.fill(row , 0) ;
         }
-
-        return solve(0, nums, true);
+        for(int i = 1  ;  i <= n ;i++){
+        t[i][0] = Math.max(t[i - 1][1] - nums[i-1]  , t[i-1][0]);
+        t[i][1] = Math.max(t[i-1][0] + nums[i- 1] , t[i-1][1]) ;
+        }
+        return  Math.max(t[n][0] , t[n][1]) ;
     }
 }
