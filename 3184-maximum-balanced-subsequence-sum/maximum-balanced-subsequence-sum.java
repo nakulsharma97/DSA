88e849fa475c746c04f1215674b4b7be097bbc57
Nakul Sharma <nakulsharma978397@gmail.com>
2026-07-28 13:06:12 +0530

    Time: 319 ms (19.51%) | Memory: 93.9 MB (57.32%) - LeetSync

--- 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.java
+++ 3184-maximum-balanced-subsequence-sum/maximum-balanced-subsequence-sum.java
@@ -1,30 +1,32 @@
 class Solution {
     public long maxBalancedSubsequenceSum(int[] nums) {
-        int n = nums.length;
-        int [] arr = new int[n];
-        for(int i = 0; i<n; i++){
-            arr[i] = nums[i]-i;
+        int n = nums.length ;
+        int arr[] = new int[n] ;
+        for(int i = 0 ; i < n;i++){
+            arr[i] = nums[i] - i  ; 
         }
-        TreeMap<Integer, Long> map = new TreeMap<>();
-        long ans = Integer.MIN_VALUE;
-        for(int i = 0; i<n; i++){
-            if(nums[i]<=0){
-                ans = Math.max(ans, nums[i]);
+        TreeMap<Integer , Long>  map  = new TreeMap<>() ;
+        long ans  = Long.MIN_VALUE ;
+        int max =  0 ;
+        for(int i =0 ; i < n ;i++){
+            if(nums[i] <= 0){
+                ans = Math.max(ans , nums[i]) ;
             }
-            else{
-                long temp = nums[i];
-                if(map.floorKey(arr[i])!=null){
+            else {
+                long temp = nums[i] ;
+                if(map.floorKey(arr[i]) != null ){
                     temp += map.get(map.floorKey(arr[i]));
                 }
-                while(map.ceilingKey(arr[i])!=null && map.get(map.ceilingKey(arr[i]))<temp){
+                while(map.ceilingKey(arr[i]) != null && map.get(map.ceilingKey(arr[i])) < temp ){
                     map.remove(map.ceilingKey(arr[i]));
+                } 
+                if(map.floorKey(arr[i]) == null || map.get(map.floorKey(arr[i])) < temp){
+                    map.put(arr[i] , temp) ;
                 }
-                if(map.floorKey(arr[i])==null || map.get(map.floorKey(arr[i]))<temp){
-                    map.put(arr[i], temp);
-                }
-                ans = Math.max(ans, temp);
+                ans = Math.max(ans , temp ) ;
+
             }
         }
-        return ans;
-    }
+        return ans  ;
+    } 
 }
