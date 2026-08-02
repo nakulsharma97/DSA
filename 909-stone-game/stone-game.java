636a0f1bcc85ac1b62d1a3dc94572e0cf4356591
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-02 12:11:21 +0530

    Time: 7 ms (21.79%) | Memory: 46.7 MB (16.42%) - LeetSync

--- 909-stone-game/stone-game.java
+++ 909-stone-game/stone-game.java
@@ -1,5 +1,28 @@
 class Solution {
+    int dp[][] ;
     public boolean stoneGame(int[] piles) {
-        return true;
+        int n = piles.length ;
+        int sum = 0 ;
+        dp = new int[n][n] ;
+        for(int row[] : dp){
+            Arrays.fill(row , 0) ;
+        }
+        for(int i = 0 ; i < n ;i++){
+            sum += piles[i] ;
+        }
+        int alice = solve(0 , n-1 , piles) ;
+        return alice > sum / 2 ;
     }
+    public int solve(int i , int j , int[] piles){
+        if(i > j){
+            return 0 ;
+        }
+        if(dp[i][j] != 0){
+            return dp[i][j] ;
+        }
+        int takei = piles[i] + Math.min(solve(i + 2 , j , piles) , solve(i + 1, j - 1 , piles)) ;
+        int takej = piles[j] + Math.min(solve(i + 1 , j - 1 , piles) , solve(i , j- 2 , piles)) ;
+        return dp[i][j] = Math.max(takei , takej) ;
+    }
+    
 }
