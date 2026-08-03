fa677aaea91d8780fe23e9cca3f9e6c58cad32e4
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-03 17:09:40 +0530

    Time: 4 ms (94.49%) | Memory: 163.8 MB (12.34%) - LeetSync

--- 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
+++ 3849-equal-sum-grid-partition-i/equal-sum-grid-partition-i.java
@@ -1,38 +1,34 @@
 class Solution {
     public boolean canPartitionGrid(int[][] grid) {
-        int n = grid.length  ;
-        int m = grid[0].length ;
-        long rows[] = new long[n] ;
-        long cols[] = new long[m] ;
-        long total = 0 ;
-        for(int i = 0 ; i < n ;i++){
-            for(int j = 0  ; j < m ;j++){
-                total += grid[i][j] ;
-
-                rows[i] += grid[i][j] ;
-                cols[j] += grid[i][j] ;
-            }
-
+     int n = grid.length ;
+     int m = grid[0].length ;
+     long total = 0 ;
+     long row[] = new long[n] ;
+     long col[] = new long[m] ;
+     for(int i = 0 ; i < n;i++){
+        for(int j = 0 ; j < m ;j++){
+            total += grid[i][j] ;
+            row[i] += grid[i][j] ;
+            col[j] += grid[i][j] ;
         }
-        if(total % 2 != 0){
-            return false ;
-        }
-        // for upper 
-
-        long upper = 0 ;
-        for(int i = 0 ; i < n -1 ;i++){
-           upper += rows[i] ;
-           if(upper == total - upper){
-            return true ;
-           }
-        }
-         long left = 0 ;
-        for(int i = 0 ; i < m -1 ;i++){
-           left += cols[i] ;
-           if(left == total - left){
-            return true ;
-           }
-        }
+     } 
+     if(total % 2 != 0){
         return false ;
+     }
+     long upper = 0  ;
+     for(int i = 0 ; i < n - 1 ;i++){
+         upper += row[i] ;
+        if(upper == total - upper){
+            return true ;
+        }
+     }  
+     long  left = 0  ;
+     for(int i = 0 ;  i < m -1 ;i++){
+        left += col[i] ;
+        if(left == total - left){
+            return true ;
+        }
+     }
+    return false ;
     }
 }
