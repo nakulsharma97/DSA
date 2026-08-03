bf0960f9dd02370f21a65add18f4ef0c1947f45d
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-03 19:27:10 +0530

    Time: 260 ms (71.20%) | Memory: 272.5 MB (75.20%) - LeetSync

--- 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
+++ 3850-equal-sum-grid-partition-ii/equal-sum-grid-partition-ii.java
@@ -1,8 +1,6 @@
 class Solution {
-   
-
     public boolean canPartitionGrid(int[][] grid) {
-        long total = 0 ;
+        long total = 0;
         int m = grid.length;
         int n = grid[0].length;
         for (int i = 0; i < m; i++) {
@@ -10,58 +8,59 @@ class Solution {
                 total += grid[i][j];
             }
         }
-
         if (check(grid, total) || check(reverse(grid), total)) {
             return true;
         }
-        int[][] transpos = transpose(grid);
-        return check(transpos, total) || check(reverse(transpos), total);
+        int[][] tra = transpose(grid);
+        return check(tra, total) || check(reverse(tra), total);
     }
 
     public boolean check(int[][] A, long total) {
-        Set<Long> seen = new HashSet<>();
         int m = A.length;
         int n = A[0].length;
-        long top = 0;
+        long upp =0 ;
+        Set<Long> seen = new HashSet<>() ;
         for (int i = 0; i < m; i++) {
-            for (int j = 0; j < n; j++) {
-                top += (long) A[i][j];
-                seen.add((long) A[i][j]);
+            for (int j = 0; j < n ; j++) {
+            seen.add((long)A[i][j]) ;         
+            upp += (long)A[i][j] ;
             }
-            long bottom = total - top;
-            long diff = top - bottom;
-            if (diff == 0)
-                return true;
+            long bottom = total - upp  ;
+            long diff = upp -bottom ;
+            if(diff == 0){
+                return true ;
+            }
+            if(diff == A[0][0] || diff == A[0][n-1] || diff == A[i][0]){
+                return true ;
 
-            if (diff == A[0][0] || diff == A[0][n - 1] || diff == A[i][0]) {
-                return true;
             }
-            if(n > 1  && i > 0 && seen.contains(diff)){
+            if(n > 1 &&  i > 0 && seen.contains(diff) ){
                 return true ;
             }
         }
-        return false;
+        return false ;
     }
 
-    private int[][] reverse(int[][] A) {
-        int[][] res = new int[A.length][];
-        for (int i = 0; i < A.length; i++) {
-            res[i] = A[A.length - 1 - i];
+    public int[][] reverse (int [][] A){
+        int n =A.length ;
+        int m =A[0].length ;
+        int [][] res = new int[n][m] ;
+        for(int i = 0 ; i< n ;i++){
+            res[i] = A[A.length -1 - i] ;
         }
-
-        return res;
+        return res ;
     }
 
-    private int[][] transpose(int[][] A) {
-        int r = A.length, c = A[0].length;
-        int[][] res = new int[c][r];
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++) {
+    public int[][] transpose(int[][] A) {
+        int n = A.length;
+        int m = A[0].length;
+        int[][] res = new int[m][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
                 res[j][i] = A[i][j];
             }
         }
-
         return res;
     }
+
 }
