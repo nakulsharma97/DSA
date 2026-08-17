c3aad0a5f33c792deb4d2ba27564227699c1de90
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-17 16:47:58 +0530

    Time: 242 ms (73.08%) | Memory: 47.8 MB (49.28%) - LeetSync

--- 1685-stone-game-v/stone-game-v.java
+++ 1685-stone-game-v/stone-game-v.java
@@ -1,44 +1,40 @@
 class Solution {
+    int prefix[] ;  
     int n ;
     int dp[][] ;
-    int prefix[] ;
     public int stoneGameV(int[] stoneValue) {
         n = stoneValue.length ;
         dp = new int[n][n] ;
-        int sum = 0 ;
-        for(int i = 0 ; i < n;i++){
-            sum += stoneValue[i] ;
-        }
+        prefix = new int[n+1] ;
         for(int row[] : dp){
-            Arrays.fill(row , -1)  ;
+            Arrays.fill(row , -1) ;
         }
-         prefix = new int[n+1] ;
-        for(int  i = 0; i <  n;i++){
-            prefix[i+1] = prefix[i]  + stoneValue[i] ;
+        for(int i = 0 ; i < n ;i++){
+            prefix[i+1] = prefix[i] + stoneValue[i] ;
         }
-        return solve(0 , n-1, stoneValue) ;
+        return solve(0 , n -1 , stoneValue) ;
     }
-    public int solve(int left , int right , int [] stoneValue){
-        if(right == left){
+    public int solve(int left , int right , int[] stoneValue){
+        if(left == right){
             return 0 ;
         }
         if(dp[left][right] != -1){
-            return dp[left][right] ;
+            return dp[left][right]  ;
+       }
+       int ans  = 0 ;
+       for(int  k = left ; k < right ;k++){
+        int leftsum = prefix[k+1]  - prefix[left] ;
+        int rightsum = prefix[right +1] - prefix[k+1] ;
+        if(leftsum < rightsum){
+            ans = Math.max(ans , leftsum + solve(left , k  , stoneValue)) ;
         }
-        int ans = 0 ;
-        for(int k  = left ; k < right ;k++){
-            int leftsum = prefix[k+1] - prefix[left] ;
-            int rightsum = prefix[right +1] - prefix[k+1];
-            if(leftsum < rightsum){
-               ans = Math.max(ans , leftsum + solve(left , k  , stoneValue));
-            }
-            else if(rightsum < leftsum){
-                ans = Math.max(ans , rightsum + solve(k+1 , right , stoneValue));
-            }
-            else {
-                ans = Math.max(ans , Math.max(leftsum + solve(left , k  ,stoneValue) , rightsum + solve(k+1 , right , stoneValue)) ) ;
-            }
+        else if(rightsum < leftsum){
+            ans = Math.max(ans , rightsum + solve(k +1 , right , stoneValue)) ;
         }
-        return dp[left][right] = ans ;
+        else{
+            ans = Math.max(ans , Math.max(rightsum + solve(k +1 , right , stoneValue) , leftsum + solve(left , k ,stoneValue))) ;
+        }
+       }
+       return dp[left][right] = ans ;
     }
 }
