8c6f45a1b895bf63ddb123280d24437c26f61005
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-19 17:42:09 +0530

    Time: 24 ms (50.00%) | Memory: 53.6 MB (29.74%) - LeetSync

--- 1487-cinema-seat-allocation/cinema-seat-allocation.java
+++ 1487-cinema-seat-allocation/cinema-seat-allocation.java
@@ -1,51 +1,38 @@
 class Solution {
     public int maxNumberOfFamilies(int n, int[][] reservedSeats) {
-
-        HashMap<Integer, List<Integer>> map = new HashMap<>();
-
-        // row -> reserved seats
-        for (int i = 0; i < reservedSeats.length; i++) {
-
-            int row = reservedSeats[i][0];
-            int seat = reservedSeats[i][1];
-
-            if (!map.containsKey(row)) {
-                map.put(row, new ArrayList<>());
+        int a = reservedSeats.length ;
+        HashMap<Integer , List<Integer>> map = new HashMap<>() ;
+        for(int i = 0 ; i< a ;i++){
+            int row = reservedSeats[i][0] ;
+            int seat = reservedSeats[i][1] ;
+            if(!map.containsKey(row)){
+                map.put(row , new ArrayList<>()) ;
             }
-
-            map.get(row).add(seat);
+            map.get(row).add(seat) ;
         }
-
-        // Completely empty rows
-        int ans = (n - map.size()) * 2;
-
-        for (int row : map.keySet()) {
-
-            List<Integer> seats = map.get(row);
-            Collections.sort(seats);
-            boolean left = true;
-            boolean right = true;
-            boolean mid = true;
-            for (int c : seats) {
-                if (c >= 2 && c <= 5)
-                    left = false;
-                if (c >= 4 && c <= 7)
-                    mid = false;
-                if (c >= 6 && c <= 9)
-                    right = false;
+        int ans = (n - map.size()) * 2  ;
+        for(int s : map.keySet()){
+            List<Integer> seats = map.get(s) ;
+            boolean left = true ;
+            boolean mid = true ;
+            boolean right = true ;
+            for(int c : seats){
+                if(c >= 2  && c <= 5) left = false ;
+                if(c >= 4 && c <= 7) mid = false  ;
+                if(c >= 6 && c <= 9) right = false  ;
             }
-            int count = 0;
-            if (left && right) {
-                count = 2;
-            } else if (left || right || mid) {
-                count = 1;
-            } else {
-                count = 0;
+            int count  ;
+            if(left && right )  {
+                count = 2 ;
             }
-
-            ans += count;
+            else if(left || right || mid){
+                count  = 1  ;
+            }
+            else{
+                count = 0  ;
+            }
+            ans += count ;
         }
-
-        return ans;
+        return ans ;
     }
 }
