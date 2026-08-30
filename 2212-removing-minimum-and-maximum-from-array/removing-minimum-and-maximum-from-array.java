c64ca052abfe6e6647e2c159e11026271d27c4c3
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-30 23:27:22 +0530

    Time: 3 ms (61.82%) | Memory: 86.4 MB (92.73%) - LeetSync

--- 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
+++ 2212-removing-minimum-and-maximum-from-array/removing-minimum-and-maximum-from-array.java
@@ -1,36 +1,33 @@
 class Solution {
     public int minimumDeletions(int[] nums) {
         int n = nums.length;
-        int left = 0;
-        int right = 0;
-        
+
+        int minIndex = 0;
+        int maxIndex = 0;
+
         for (int i = 1; i < n; i++) {
-            if (nums[i] < nums[left])
-                left = i;
-                
-            if (nums[i] > nums[right])
-                right = i;
+            if (nums[i] < nums[minIndex]) {
+                minIndex = i;
+            }
+
+            if (nums[i] > nums[maxIndex]) {
+                maxIndex = i;
+            }
         }
-        
-        if (left < right) {
-            int temp = left;
-            left = right;
-            right = temp;
-        }
-            
-        int ans = n;
-        
-        for (int i = 0; i <= n; i++) {
-            int extra = 0;
-            
-            if (right >= i)
-                extra = n - right;
-            else if (left >= i)
-                extra = n - left;
-                
-            ans = Math.min(ans, i + extra);
-        }
-        
-        return ans;
+
+        int left = Math.min(minIndex, maxIndex);
+        int right = Math.max(minIndex, maxIndex);
+
+        int deleteFromLeft = right + 1;
+
+        int deleteFromRight = n - left;
+
+        int deleteFromBoth =
+                (left + 1) + (n - right);
+
+        return Math.min(
+                deleteFromLeft,
+                Math.min(deleteFromRight, deleteFromBoth)
+        );
     }
 }
