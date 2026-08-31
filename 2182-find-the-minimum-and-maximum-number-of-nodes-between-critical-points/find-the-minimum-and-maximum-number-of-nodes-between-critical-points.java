b5ccd28ba54dc2ed0cd321eb121d7f0a6cd28cac
Nakul Sharma <nakulsharma978397@gmail.com>
2026-08-31 13:16:30 +0530

    Time: 23 ms (6.05%) | Memory: 118.7 MB (5.10%) - LeetSync

--- 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
+++ 2182-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/find-the-minimum-and-maximum-number-of-nodes-between-critical-points.java
@@ -10,33 +10,31 @@
  */
 class Solution {
     public int[] nodesBetweenCriticalPoints(ListNode head) {
-        List<Integer> list = new ArrayList<>() ;
         ListNode temp = head ;
+        List<Integer> list = new ArrayList<>() ;
         while(temp != null){
             list.add(temp.val) ;
             temp =temp.next ;
         }
-        int n = list.size() ;
+        int n = list.size()  ;
         List<Integer> critical = new ArrayList<>() ;
-        for(int i = 1 ; i < n -1 ;i++){
-            if(list.get(i) > list.get(i -1) && list.get(i) > list.get(i +1)){
-                critical.add(i) ;
-            }
-            if(list.get(i) < list.get(i-1) && list.get(i) < list.get(i +1)){
-                critical.add(i) ;
-            }
+        for(int i = 1 ; i< n - 1 ;i++){
+          if(list.get(i) > list.get(i-1) && list.get(i)  > list.get(i +1)){
+            critical.add(i) ;
+          }
+          if(list.get(i) < list.get(i -1) && list.get(i)  <list.get(i +1)){
+            critical.add(i) ;
+          }
         }
         int m = critical.size() ;
         if(m < 2){
-            return new int[]{-1, -1} ;
+            return new int[]{-1 ,-1} ;
         }
-        int min = Integer.MAX_VALUE ;
         int max = critical.get(m-1) - critical.get(0) ;
+        int min = Integer.MAX_VALUE ;
         for(int i = 1 ; i < m ;i++){
-            min = Math.min(min , critical.get(i) - critical.get(i-1)) ;
+            min =Math.min(min , critical.get(i) - critical.get(i-1)) ;
         }
-        return new int[]{min , max};
-        
-
+        return new int[]{min, max} ;
     }
 }
