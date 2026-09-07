474511ecf30bfbf0cb2ac0265a2bd35cbb9ffbd9
Nakul Sharma <nakulsharma978397@gmail.com>
2026-09-07 09:54:39 +0530

    Time: 19 ms (69.18%) | Memory: 53.6 MB (81.56%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -2,30 +2,24 @@ class Solution {
     public int numDistinct(String s, String t) {
         int n = s.length() ;
         int m = t.length() ;
-        int dp[][] =new int[n][m];
-        for(int row[]  : dp){
-            Arrays.fill(row , -1);
+        int dp[][]  = new int[n+1][m + 1] ;
+        for(int i = 0 ; i <= n ;i++){
+            dp[i][0] = 1 ;
         }
-        return solve(s , t , n , m , 0 , 0 , dp) ;
-    }
-    public int solve(String s , String t , int n , int m , int i ,int j ,int dp[][]){
-        if(j == m){
-            return 1  ;
+        for(int i = 1 ; i <= m ;i++){
+            dp[0][i] = 0 ;
         }
-        if(i == n){
-            return 0 ;
+        for(int i = 1 ; i <=n;i++){
+            for(int j = 1 ; j <= m ;j++){
+                if(s.charAt(i-1) == t.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i-1][j] ;
+                }
+                else {
+                    dp[i][j] = dp[i-1][j] ;
+                }
+            }
         }
-        if(dp[i][j] != -1){
-            return dp[i][j] ;
-        }
-        if(s.charAt(i) == t.charAt(j)){
-            int take = solve(s , t , n , m , i + 1 , j + 1, dp) ;
-            int skip = solve(s , t , n , m , i +1 , j , dp) ;
-            dp[i][j] = take + skip ;
-        }
-        else {
-              dp[i][j] = solve(s , t , n , m , i + 1 , j , dp) ;
-        }
-        return dp[i][j] ;
+        
+        return dp[n][m] ;
     }
 }
