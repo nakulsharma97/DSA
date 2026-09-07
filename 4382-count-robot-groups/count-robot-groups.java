06304a45cc60f1eb06d866f023a2833706a0bae5
Nakul Sharma <nakulsharma978397@gmail.com>
2026-09-07 16:23:36 +0530

    Time: 11 ms (100.00%) | Memory: 103 MB (100.00%) - LeetSync

---
+++ 4382-count-robot-groups/count-robot-groups.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countGroups(int[] position, int[] speed, int distance) {
+        int n = position.length  ;
+            Deque<Integer> q = new ArrayDeque<>();
+            for(int i = 0 ; i < n ;i++){
+                if(i > 0 && position[i] - position[i-1]  <= distance){
+                    q.pop() ;
+                }
+                q.push(speed[i]) ;
+            }
+            int ans = 0 ;
+            int minspeed = Integer.MAX_VALUE ;
+            for(int i = q.size() -1 ; i >= 0 ; i--){
+                int a = q.pop() ;
+                if(a <= minspeed){
+                    ans++ ;
+                    minspeed = a ; 
+                }
+            }
+            return ans  ;
+    }
+} 
